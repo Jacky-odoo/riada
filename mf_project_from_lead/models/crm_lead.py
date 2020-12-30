4fa58c06010a65b68b8fe64f29bd00f9108e4ebe
fouad <mohm.foad.95@gmail.com>
2020-12-30 03:14:38 +0200

    [UDATE] mf_activity_update : unlink activity, [ADD] mf_project_from_lead : add module to create project from lead

--- mf_project_from_lead/models/crm_lead.py
+++ mf_project_from_lead/models/crm_lead.py
@@ -22,7 +22,9 @@ class CrmLead(models.Model):
                 'name': 'المرجع',
                 'stage_id': self.env.ref('mf_project_from_lead.stage_start').id,
                 'project_id': project.id,
+                'partner_id': rec.partner_id.id,
                 'user_id': self.env.uid,
+                'description': rec.description,
             })
             all_messages_crm = self.env['mail.message'].search(
                 ["&", ('res_id', "=", rec.id), ('model', "=", "crm.lead"), ('message_type', '!=', 'user_notification')])
