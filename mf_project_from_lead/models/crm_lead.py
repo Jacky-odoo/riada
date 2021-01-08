bc0a15f4a0640082fc21298cf75e8f6a37198d72
fouad <mohm.foad.95@gmail.com>
2021-01-09 01:57:55 +0200

    [UPDATE] mf_activity_update : except mail from copy to project

--- mf_project_from_lead/models/crm_lead.py
+++ mf_project_from_lead/models/crm_lead.py
@@ -11,7 +11,7 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     can_be_project = fields.Boolean(string="", )
-    have_project = fields.Boolean(string="", default=False,copy=False)
+    have_project = fields.Boolean(string="", default=False, copy=False)
 
     def create_project(self):
         for rec in self:
@@ -29,19 +29,20 @@ class CrmLead(models.Model):
                 'description': rec.description,
             })
             all_messages_crm = self.env['mail.message'].search(
-                ["&", ('res_id', "=", rec.id), ('model', "=", "crm.lead")],order='create_date asc')
+                ["&", ('res_id', "=", rec.id), ('model', "=", "crm.lead"),], order='create_date asc')
             for message in all_messages_crm:
-                print(message.message_type)
-                message.sudo().copy(
-                    {'model': 'project.task', 'res_id': task.id,
-                     'email_layout_xmlid': message.email_layout_xmlid, 'message_type': message.message_type,
-                     'create_date': message.create_date,
-                     })
-            all_attach_crm = self.env['ir.attachment'].search(
-                ["&", ('res_id', "=", rec.id), ('res_model', "=", "crm.lead")])
-            for attach in all_attach_crm:
-                attach.sudo().with_context(no_document=True).copy(
-                    {'res_model': 'project.project', 'res_id': project.id,'type': 'binary',})
+                if message.notification_ids.notification_status not in ['exception','canceled'] :
+                    print(message.message_type, message.body,message.notification_ids,message.notification_ids.notification_status)
+                    message.sudo().copy(
+                        {'model': 'project.task', 'res_id': task.id,
+                         'email_layout_xmlid': message.email_layout_xmlid, 'message_type': message.message_type,
+                         'create_date': message.create_date, 'partner_ids': message.partner_ids,
+                         })
+                all_attach_crm = self.env['ir.attachment'].search(
+                    ["&", ('res_id', "=", rec.id), ('res_model', "=", "crm.lead")])
+                for attach in all_attach_crm:
+                    attach.sudo().with_context(no_document=True).copy(
+                        {'res_model': 'project.project', 'res_id': project.id, 'type': 'binary', })
 
     def open_project(self):
         project = self.env['project.project'].search([('lead_id', '=', self.id)], limit=1).id
