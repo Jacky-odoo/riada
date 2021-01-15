92eb08982e28242e56e9398e8ce7299210cd3496
fouad <mohm.foad.95@gmail.com>
2021-01-16 00:07:09 +0200

    [UPDATE] mf_violations : archive violations

--- mf_violations/models/models.py
+++ mf_violations/models/models.py
@@ -15,3 +15,14 @@ class HrViolation(models.Model):
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", tracking=True, required=False,default=lambda self: self.env.user.employee_id )
     violation_date = fields.Date(string="", required=False, tracking=True)
     description = fields.Text(string="", required=False, tracking=True)
+    active = fields.Boolean(default=True,)
+    display = fields.Boolean(default=True,)
+
+
+
+    def archive(self):
+        for rec in self:
+            if rec.display == True:
+                rec.display = False
+            else:
+                rec.display = True
