c65e9fd36c9cf635e4f7b0cee6febf105e79f083
mfhm95 <mohmd.foad95@gmail.com>
2021-02-05 01:22:10 +0200

    [ADD] attachment_activity_done

---
+++ attachment_activity_done/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Attachment on activity feedback",
+    'summary': """
+        Upload Attachment on activity feedback
+    """,
+    'description': """
+        Upload Attachment on activity feedback
+    """,
+    'author': "Kareem Gaber",
+    'website': "https://www.facebook.com/kareem.jaber.hossein",
+    'contributors': [
+        'Kareem Gaber <k.japer.99@gmail.com>',
+    ],
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['mail'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'qweb': ['static/src/xml/feedback_template.xml'],
+    'application': True,
+}
